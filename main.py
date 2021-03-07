934201b367e63413e0bd77112ddcedf4d882d91f
lawsonnuland <lawson.nuland@gmail.com>
2021-03-07 17:18:34 -0500

    Testing python.

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-#fuck python get bitches
-#get bitches fuck python
+#dnd inventory system management
+
+#user input characters
+characters = []
+print("input character name")
+characters.append(input())
+
+print(characters[0])
+#user input items
+
