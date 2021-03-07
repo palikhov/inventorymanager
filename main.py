a6f96b06c18f87d50b2711c3d6a3c78c5c6c6fdc
lawsonnuland <lawson.nuland@gmail.com>
2021-03-07 18:14:59 -0500

    Bugged. Sent for testing.

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-#dnd inventory system management
+# dnd inventory system management
+# user input characters
+# characters = []
+# print("input character name")
+# characters.append(input())
+# characters.append(input())
+# characters.append(input())
+# print(characters)
+# user input items
 
-#user input characters
-characters = []
-print("input character name")
-characters.append(input())
+while True:
+    print("What do you wanna do? 1 for 2 for 3 for 4 for quit")
+    user_input = input()
+    if user_input == 1:
+        print("any")
+        break
+    elif user_input == 2:
+        print()
+    elif user_input == 3:
+        print()
+    elif user_input == 4:
+        break
+    else:
+        continue
+    False
 
-print(characters[0])
-#user input items
-
+print("done")
