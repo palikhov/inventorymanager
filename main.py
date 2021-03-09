064fa692068e7ce470e9ec0b2912a3a27116a4e7
lawsonnuland <lawson.nuland@gmail.com>
2021-03-08 21:00:41 -0500

    Found the bug, built out the if else tree some.

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 # dnd inventory system management
 # user input characters
-# characters = []
+characters = []
 # print("input character name")
-# characters.append(input())
-# characters.append(input())
-# characters.append(input())
 # print(characters)
 # user input items
 
 while True:
-    print("What do you wanna do? 1 for 2 for 3 for 4 for quit")
-    user_input = input()
+    print("What do you wanna do? 1 for adding a character. 2 for adding to the inventory of a character. 3 to print the list of characters. 4 for quit")
+    user_input = int(input())
     if user_input == 1:
-        print("any")
-        break
-    elif user_input == 2:
-        print()
-    elif user_input == 3:
-        print()
-    elif user_input == 4:
-        break
-    else:
+        print("Add character")
+        characters.append(input())
         continue
-    False
+    elif user_input == 2:
+        print("What character do you want to add inventory items to?")
+    elif user_input == 3:
+        print(characters)
+    elif user_input == 4:
+        print("trash")
+    else:
+        break
+
 
 print("done")
